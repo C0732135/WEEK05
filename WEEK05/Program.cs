bb3db06c4a4f8aa81f35c25256ccbef969a45f43
visualstudio <visualstudio@122-PC06>
2019-02-08 19:06:07 -0500

    7 pm commit

--- WEEK05/Program.cs
+++ WEEK05/Program.cs
@@ -30,12 +30,27 @@ namespace WEEK05
 
         public void run()
         {
-            FirstFloor = new Node();
+            FirstFloor = new Node();//memory allocation
+            SecondFloor = new Node();
+            ThirdFloor = new Node();
+            FourthFloor = new Node();
             FirstFloor.FloorNumber = "First Floor";
             Console.WriteLine("Floor number is{0}", FirstFloor.FloorNumber);
+          
             FirstFloor.elevatorUp = SecondFloor;
             SecondFloor.FloorNumber = "Second Floor";
             Console.WriteLine("Floor number is{0}", SecondFloor.FloorNumber);
+
+            SecondFloor.elevatorUp = ThirdFloor;
+            ThirdFloor.FloorNumber = "ThirdFloor";
+            Console.WriteLine("Floor number is{0}", ThirdFloor.FloorNumber);
+
+            ThirdFloor.elevatorUp = FourthFloor;
+            FourthFloor.FloorNumber = "Fourth floor";
+            Console.WriteLine("Floor number is{0}", FourthFloor.FloorNumber);
+
+            FourthFloor.elevatorUp = null;
+           
         }
     }
 }
